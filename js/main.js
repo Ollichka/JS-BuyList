09d087ca798cbf6f93432b62595c1d336ec6734c
Ollichka <salnikova.olia@gmail.com>
2016-01-29 21:21:13 +0200

    all except editing name

--- js/main.js
+++ js/main.js
@@ -1,9 +1,103 @@
 $(function(){
     var LIST1 = $('.listOfItems');
-    var ITEM_TEMPLATE = $('.oneItem').html();
-    var LIST2 = $('.list2');
-    var ITEM  = $('.oneOrd').html();
+    var LIST2 = $('.select');
+    var LIST3 = $('.buyIT');
+    var title;
     $(".text1").focus();
+
+
+    function createItem(title){
+       var item = $('<span class="oneItem">'+
+            '<input type="text" class="prod" value="'+title+'">'+
+            '<div class="a1">'+
+            '<button type="button" class="buttonminus">-</button>'+
+           '<span class="count">1</span>'+
+            '<button type="button" class="buttonplus">+</button>'+
+            '</div>'+
+            '<button type="button" class="notBought">Не купленно</button>'+
+            '<button type="button" class="bought">Куплено</button>'+
+            '<button type="button" class="delete">X</button>'+
+            '<div class="lines"></div>'+
+            '</span>');
+
+        LIST1.append(item);
+        var item2 = $('<span class="ord"'+'<span class="nameP">'+title+
+            '<span class="num">1</span>'+'</span>'+'</span>');
+        LIST2.append(item2);
+
+        var plus = item.find(".buttonplus");
+        var minus = item.find(".buttonminus");
+        var buy =  item.find(".bought");
+        var nobuy = item.find(".notBought");
+        var num = item2.find(".num");
+        var x = item.find(".delete");
+        var count = item.find(".count");
+        var kil = count.text();
+        var name = item.find(".prod");
+        var name2 = item.find(".nameP");
+
+        $(minus).prop("disabled", true);
+        $(nobuy).hide();
+
+        $(minus).click(function(){
+            $(count).text(--kil);
+            $(num).text(kil);
+            if(kil===1){
+                $(minus).css("background-color","rgba(226, 37, 19, 0.34)");
+                $(minus).prop("disabled", true);
+            }
+            if((kil>1)) {
+                $(minus).css("background-color","red");
+                $(minus).prop("disabled", false);
+            }
+        });
+
+
+        $(plus).click(function(){
+            $(count).text(++kil);
+            $(num).text(kil);
+            if(kil===1){
+                $(minus).css("background-color","rgba(226, 37, 19, 0.34)");
+                $(minus).prop("disabled", true);
+            }
+            if((kil>1)) {
+                $(minus).css("background-color","red");
+                $(minus).prop("disabled", false);
+            }
+        });
+
+        $(buy).click(function(){
+            name.css("text-decoration","line-through");
+            minus.hide();
+            plus.hide();
+            buy.hide();
+            x.hide();
+            nobuy.show();
+            name2.css("text-decoration","line-through");
+            num.css("text-decoration","line-through");
+            LIST3.append(item2);
+        });
+
+        x.click(function () {
+            item.hide();
+            item2.hide();
+            item2.remove();
+            item.remove();
+        });
+
+        $(nobuy).click(function(){
+            name.css("text-decoration","none");
+            minus.show();
+            buy.show();
+            x.show();
+            plus.show();
+            name2.css("text-decoration","none");
+            num.css("text-decoration","none");
+            LIST2.append(item2);
+            nobuy.hide();
+        });
+
+    }
 
     $(".text1").keyup(function (event) {
         if (event.keyCode == 13) {
@@ -11,85 +105,15 @@ $(function(){
         }
     });
 
-    var title;
     $('.button').click(function () {
         title = $('.text1').val();
-        add(title);
+        createItem(title);
         $('.text1').val("");
         $('.text1').focus();
 
     });
 
-    function add(title){
-        var node = $(ITEM_TEMPLATE);
-        var count  = true;
-        if(title) {
-            LIST1.find(".listOfItems").each(function (index, item) {
-                item = $(item);
-                var name = item.find(".prod").val();
-                if (title == name) {
-                    // item.find(".buttonplus").click();
-                    count = false;
-                    return;
-                }
-            });
-        }
-        if(!count) return;
-        node.find(".prod").attr("value",title);
-        LIST1.append(node);
-        addLeft(title,node);
-    }
-
-    function addLeft( title,node){
-        var node1 = $(ITEM);
-        node1.find(".name").text(title);
-        LIST2.append(node1);
-    }
-
-    $(".button2").click(function(){
-        var node = $(ITEM_TEMPLATE);
-        node.find(".prod").css("text-decoration","line-through");
-    });
-
-    $('.button3').click(function () {
-        var node = $(ITEM_TEMPLATE);
-        $(this).parent().hide();
-        node.remove();
-        var node1 = $(ITEM);
-    });
-
-    var count = 1;
-    $(".buttonminus").prop("disabled", true);
-    $(".buttonplus").click(function(title){
-        $(".count").text(++count);
-        $(".num").text(count);
-        if(count===1){
-            $(".buttonminus").css("background-color","rgba(226, 37, 19, 0.34)");
-            $(".buttonminus").prop("disabled", true);
-            console.log("button blocked");
-        }
-        if((count>1)) {
-            $(".buttonminus").css("background-color","red");
-            $(".buttonminus").prop("disabled", false);
-        }
-    });
-    $(".buttonminus").click(function(){
-        $(".count").text(--count);
-        $(".num").text(count);
-        if(count===1){
-            $(".buttonminus").css("background-color","rgba(226, 37, 19, 0.34)");
-            $(".buttonminus").prop("disabled", true);
-            console.log("button blocked");
-        }
-        if((count>1)) {
-            $(".buttonminus").css("background-color","red");
-            $(".buttonminus").prop("disabled", false);
-        }
-        console.log("count-");
-    });
-
-    $(".button3").click(function(){
-        $(".oneItem").remove();
-
-    });
+    createItem("PineApple");
+    createItem("Coconut");
+    createItem("Melon");
 });
